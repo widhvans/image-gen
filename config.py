751b4fc07d711791426da93c6b672985f4ac8d5d
widhvans <110589162+widhvans@users.noreply.github.com>
2025-03-28 13:30:22 +0530

    top noach quality improved

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 API_ID = "27461953"      # Apna API ID yahan daalo
-API_HASH = "8a19a6a007044ff7b41ada4b377cdfba"  # Apna API Hash yahan daalo
+API_HASH = "8a19a6a007044ff7b41ada4b377cdfba"  # Apna API Hash yahan daal
 BOT_TOKEN = "7191299411:AAEwGLIbGF0ATvpZ1F06MrYRfNiYX1xcwIA"  # Apna Bot Token yahan daalo
