b7c49557e5c00e3099c984bb3cd692fce6902ec6
widhvans <110589162+widhvans@users.noreply.github.com>
2025-03-28 12:58:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
-
+API_ID = "your_api_id_here"      # Apna API ID yahan daalo
+API_HASH = "your_api_hash_here"  # Apna API Hash yahan daalo
+BOT_TOKEN = "your_bot_token_here"  # Apna Bot Token yahan daalo
